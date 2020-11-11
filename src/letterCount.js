641a4f642a5f893250ef63fa79e9a4f1038efcc1
AKP-13 <alexpeirson@hotmail.com>
2020-11-11 17:02:42 +0000

    Remove comment

--- src/letterCount.js
+++ src/letterCount.js
@@ -1,6 +1,3 @@
-// 'cat' -> { c: 1, a: 1, t: 1 }
-// 'better' => { b: 1, e: 2, t: 2, r: 1 }
-
 export const getLetterCount = (string) => {
     const letters = string.toLowerCase().split("");
     let letterCount = {};
