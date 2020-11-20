531cd6b3d5a9ab7e2e4d0e3a2c46e9a37ecfedc1
AKP-13 <alexpeirson@hotmail.com>
2020-11-20 21:40:55 +0000

    Finish testing

--- src/anagrams.test.js
+++ src/anagrams.test.js
@@ -1,7 +1,6 @@
 import { expect } from "chai";
 import { isAnagram } from "./anagrams";
 
-// Function that accepts two strings and tells you whether theyre anagrams of eachother
 describe("isAnagram - basic functionality", () => {
     it("returns true if the two words contain the same letters", () => {
         const actual = isAnagram("silent", "listen");
@@ -12,10 +11,21 @@ describe("isAnagram - basic functionality", () => {
         const actual = isAnagram("true", "false");
         expect(actual).to.equal(false);
     });
+
+    it("returns false if the words are the same length but contain different nunmbers of letters", () => {
+        const actual = isAnagram("no", "oo");
+        expect(actual).to.equal(false);
+    });
+
+    it("it works regardless of spaces", () => {
+        const actual = isAnagram("conversation", "voices rant on");
+        expect(actual).to.equal(true);
+    });
+
+    it("it works regardless of capital letters", () => {
+        const actual = isAnagram("yes", "YES");
+        expect(actual).to.equal(true);
+    });
 });
-
-// they must have the same letters in the same quantities
-
-// ignore spaces
 
 // ignore caps
